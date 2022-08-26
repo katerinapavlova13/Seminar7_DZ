7268b0e684b34ff1e587042435be45f0d293cecf
EkaterinaPavlova <malinakata@gmail.com>
2022-08-26 15:32:36 +0400

    Sav1

---
+++ Task47/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+double[,] CreateMatrixRndInt(int row, int col, int min, int max) 
+{
+    double[,] matrix = new double[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * (min - max) - min;
+        }
+    }
+    return matrix;
+}
+
+void PrintAMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{Math.Round(matrix[i, j], 1, MidpointRounding.AwayFromZero), 4}; ");
+            else Console.Write($"{Math.Round(matrix[i, j], 1, MidpointRounding.AwayFromZero), 4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[,] array2D = CreateMatrixRndInt(3, 4, -10, 10);
+PrintAMatrix(array2D);
---
+++ Task50/Program.cs
@@ -0,0 +1,49 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//1, 7 -> такого элемента в массиве нет
+
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintAMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3},");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -9, 9);
+PrintAMatrix(array2D);
+
+Console.WriteLine("Введите позицию элемента.");
+Console.Write("Стрoка:");
+int row1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Столбец:");
+int col1 = Convert.ToInt32(Console.ReadLine());
+
+
+
